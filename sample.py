941d91abcd91c62b7a4fe5748b0b12446790bc1f
Aaron Schlesinger <arschles@gmail.com>
2015-10-01 13:22:02 -0700

    small modification to GET

--- sample.py
+++ sample.py
@@ -1,4 +1,6 @@
 import urllib.request
+print("GET http://httpbin.org/get")
+print("\n")
 with urllib.request.urlopen('http://httpbin.org/get') as response:
    html = response.read()
    print(html)
